88d91cfd84fa7e2a6e0bf64ea9bd29894d12f348
ShabnamZ <shabnam.ziyovuddinova@gmail.com>
2019-07-01 15:50:29 -0400

    Creating db for Fitness Tracker app

---
+++ FitnessTracker.WebMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FitnessTracker.WebMVC.MvcApplication" Language="C#" %>
---
+++ FitnessTracker.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FitnessTracker.WebMVC.Startup))]
+namespace FitnessTracker.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
